d773543fd764a8606d7c03734550864051581fb0
sandeep <sr5545434@gmail.com>
2026-04-04 05:07:08 +0530

    added new fields for db users

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -1,7 +1,45 @@
 
-import { DataType,InferCreationAttributes,InferAttributes,CreationOptional, Model } from "sequelize";
-import dotenv from 'dotenv';
+import { DataType,InferCreationAttributes,InferAttributes,CreationOptional, Model, DataTypes } from "sequelize";
+import bcrypt from "bcryptjs";
+import sequelize from "../config/db.js";
 
-async class User:<InferAttributes<User>,InferCreationAttributes<User> extends Model {
+class User extends Model<InferAttributes<User>,InferCreationAttributes<User>> {
 
+    declare id:CreationOptional<number>;
+    declare name:string;
+    declare email:string;
+    declare password:string;
+    declare role:"admin"|"superadmin";
+    declare is_active:CreationOptional<boolean>;
+    declare readonly created_at:CreationOptional<Date>;
+    declare readonly updaed_at:CreationOptional<Date>;
 }
+
+User.init(
+    {
+        id:{
+            type:DataTypes.INTEGER.UNSIGNED,
+            autoIncrement:true,
+            primaryKey:true,
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+    },
+    {
+        sequelize,
+        tableName:"users",
+    }
+);
+
+export default User;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,60 @@
-import express, { NextFunction,Request,Response } from 'express';
-import dotenv from 'dotenv';
-import { testConnection } from './config/db.js';
+import express,{ Request,Response,NextFunction } from "express";
 import cors from 'cors';
+import dotenv from 'dotenv';
+import sequelize from "./config/db.js";
+import User from "./model/User.js";
 
-const app=express();
+//declare simple user model
+
 dotenv.config();
 
-app.use(express.json());
-app.use(cors({
-    origin:process.env.CLIENT_URL || "http://localhost:5173",
-    credentials:true
-}))
+async function bootstrap():Promise<void> {
 
-//mounting routes will be over here
+    //step-1: testing db connection
+    console.log("Connecting to db");
+    try {
+        await sequelize.authenticate();
+        console.log("MYsql connected successfully");
+    }
+    catch(error) {
+       console.error("My sql connection  failed:",error);
+       process.exit(0);
+    }
+
+    //step2: migrate the tables
+    console.log("synching db schema");
+    try {
+     await sequelize.sync({force:true})
+     console.log("db schema synced");
+    }
+    catch(error) {
+      console.error('Schema sync failed:');
+      process.exit(1);
+    }
+
+    const app=express();
+
+    app.use(express.json());
+    app.use(cors({
+        origin:process.env.CLIENT_URL || "http://localhost:5173",
+        credentials:true
+    }))
 
 
-//here simple api
-app.get("/",(req,res) => {
-    res.json({message:"Welcome to CRM Lite API"})
-})
+    app.get("/",(req,res) =>{
+        res.json({message:"CRM LITE API IS RUNNING"});
+    })
 
+    //global error handler
+    app.use((err:Error,req:Request,res:Response,next:NextFunction) => {
+        console.error("Unhandeld error:",err.message);
+        res.status(500).json({message:"Internal server error."});
+    });
 
-//here global error handling middleware
-app.use( (err:Error,req:Request,res:Response,next:NextFunction) => {
-   console.error('Unexpected handled Error',err.message)
-   res.status(500).json({message:"Internal Server Error"})
-})
+    const PORT=process.env.PORT || 5000;
+    app.listen(PORT,() => {
+        console.log("Server is running on port ",PORT);
+    });
+}
 
-
-//port connection
-const PORT=(process.env.PORT||5000)
-app.listen(PORT, async() => {
-    await testConnection();
-    console.log(`Server is running on port ${PORT}`);
-})
-
+bootstrap();
