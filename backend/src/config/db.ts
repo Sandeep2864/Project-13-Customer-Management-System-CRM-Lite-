43c8875e3cf4cb20dc8ad17d6310213a94cc6279
Seelam Sandeep reddy <2200030956@kluniversity.in>
2026-04-10 22:15:45 +0530

    editing small changes in db.js to connect to railway

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,27 +3,33 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize =new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME as string,
     process.env.DB_USER as string,
     process.env.DB_PASSWORD as string,
     {
-        host:process.env.DB_HOST || "localhost",
-        port:Number(process.env.DB_PORT)|| 3306,
-        dialect:"mysql",
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) || 3306,
+        dialect: "mysql",
         pool: {
-            max:10,
-            min:0,
-            acquire:30000,
-            idle:10000,
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
         },
-        logging:process.env.NODE_ENV==="development"
-        ?(sql:string) => console.log(`\n 🔍 SQL: ${sql}\n`):false,
+        // Required for many cloud hosts like Railway
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false, 
+            }
+        },
+        logging: process.env.NODE_ENV === "development"
+            ? (sql: string) => console.log(`\n 🔍 SQL: ${sql}\n`) : false,
 
         define: {
-            underscored:true,
-            timestamps:true,
-            freezeTableName:false,
+            underscored: true,
+            timestamps: true,
+            freezeTableName: false,
         },
     }
 );
