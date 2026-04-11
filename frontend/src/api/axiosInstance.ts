ab4c0f807a11c1af03c17d826f2514ffb5805e55
Seelam Sandeep reddy <2200030956@kluniversity.in>
2026-04-11 16:44:40 +0530

    Update axiosInstance.ts

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://project-13-customer-management-syst-ten.vercel.app",
+  baseURL: "https://project-13-customer-management-system-crm-lite-cteq-ovpdkqmar.vercel.app",
   withCredentials: true,
 });
 
